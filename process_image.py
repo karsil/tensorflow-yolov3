076277d2af583e462cf3bf648f20013f1d215e69
Jan Steeg <jan.n.steeg@student.fh-kiel.de>
2019-12-09 16:52:15 +0100

    added logs to image processing

---
+++ process_image.py
@@ -0,0 +1,63 @@
+#! /usr/bin/env python
+# coding=utf-8
+#================================================================
+#   Copyright (C) 2019 * Ltd. All rights reserved.
+#
+#   Editor      : VIM
+#   File name   : process_image.py
+#   Author      : YunYang1994, karsil
+#   Created date: 2019-01-20 16:06:06
+#   Description :
+#
+#================================================================
+
+import cv2
+import numpy as np
+import core.utils as utils
+import tensorflow as tf
+from PIL import Image
+
+return_elements = ["input/input_data:0", "pred_sbbox/concat_2:0", "pred_mbbox/concat_2:0", "pred_lbbox/concat_2:0"]
+pb_file         = "./yolov3_fish.pb"
+num_classes     = 2
+input_size      = 416
+graph           = tf.Graph()
+
+image_path = ""
+fileName = sys.argv[1]
+if os.path.exists(fileName):
+    image_path = os.path.abspath(fileName)
+    print("Source: ", image_path)
+else :
+    print("Missing parameter (image source)")
+    raise SystemExit
+
+original_image = cv2.imread(image_path)
+original_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB)
+original_image_size = original_image.shape[:2]
+image_data = utils.image_preporcess(np.copy(original_image), [input_size, input_size])
+image_data = image_data[np.newaxis, ...]
+
+return_tensors = utils.read_pb_return_tensors(graph, pb_file, return_elements)
+
+
+with tf.Session(graph=graph) as sess:
+    pred_sbbox, pred_mbbox, pred_lbbox = sess.run(
+        [return_tensors[1], return_tensors[2], return_tensors[3]],
+                feed_dict={ return_tensors[0]: image_data})
+
+pred_bbox = np.concatenate([np.reshape(pred_sbbox, (-1, 5 + num_classes)),
+                            np.reshape(pred_mbbox, (-1, 5 + num_classes)),
+                            np.reshape(pred_lbbox, (-1, 5 + num_classes))], axis=0)
+
+bboxes = utils.postprocess_boxes(pred_bbox, original_image_size, input_size, 0.3)
+bboxes = utils.nms(bboxes, 0.45, method='nms')
+image = utils.draw_bbox(original_image, bboxes)
+image = Image.fromarray(image)
+
+exportName = "OUT-" + fileName
+print("Done. Exporting image to ", exportName)
+image.save(exportName)
+
+
+
